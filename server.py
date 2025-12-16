357ea421aef623ccc4846b4a8c48ca08030687e7
aze <226987740+azecx@users.noreply.github.com>
2025-12-16 20:23:29 +0100

    feat: socket shit

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 import socket
 import json
 
-with open("config.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
+with open("config.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
 
 HOST = data["host"]
 PORT = data["port"]
 TOKEN = data["token"]
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server listening...")
+
+    while True: 
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connect from {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+            print(f"{addr} disconnected.")
